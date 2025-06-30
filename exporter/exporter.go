d51dc5eda7d8a58cc0f6c6bea1526f91b45851a9
mrrr61 <mrrr61@outlook.com>
2025-06-30 10:54:36 +0000

    Watch and push metrics.

---
+++ exporter/exporter.go
@@ -0,0 +1,99 @@
+package exporter
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"sync"
+	"time"
+
+	"go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetricgrpc"
+	"go.opentelemetry.io/otel/metric"
+	sdkmetric "go.opentelemetry.io/otel/sdk/metric"
+)
+
+type Reporter struct {
+	provider *sdkmetric.MeterProvider
+
+	mu       sync.Mutex
+	counter  int64
+	loss     float64
+	accuracy float64
+}
+
+func NewReporter(ctx context.Context, endpoint string) (*Reporter, error) {
+	exporter, err := otlpmetricgrpc.New(
+		ctx,
+		otlpmetricgrpc.WithInsecure(),
+		otlpmetricgrpc.WithEndpoint(endpoint),
+	)
+	if err != nil {
+		log.Fatalf("Create OTLP metric exporter failed: %v", err)
+	}
+
+	meterProvider := sdkmetric.NewMeterProvider(
+		sdkmetric.WithReader(sdkmetric.NewPeriodicReader(exporter, sdkmetric.WithInterval(60*time.Second))),
+	)
+
+	meter := meterProvider.Meter("fl-train-events")
+
+	r := &Reporter{
+		provider: meterProvider,
+	}
+
+	lossGauge, err := meter.Float64ObservableGauge("fl.training.epoch.loss")
+	if err != nil {
+		meterProvider.Shutdown(ctx)
+		return nil, fmt.Errorf("create loss gauge failed: %w", err)
+	}
+
+	accuracyGauge, err := meter.Float64ObservableGauge("fl.training.epoch.accuracy")
+	if err != nil {
+		meterProvider.Shutdown(ctx)
+		return nil, fmt.Errorf("create accuracy gauge failed: %w", err)
+	}
+
+	epochGauge, err := meter.Int64ObservableGauge("fl.training.epoch.counter")
+	if err != nil {
+		meterProvider.Shutdown(ctx)
+		return nil, fmt.Errorf("create epoch gauge failed: %w", err)
+	}
+
+	_, err = meter.RegisterCallback(
+		func(_ context.Context, o metric.Observer) error {
+			log.Printf("Meter callback triggered, Observing Epoch: %d, Loss: %.4f, Accuracy: %.4f", r.counter, r.loss, r.accuracy)
+			r.mu.Lock()
+			o.ObserveInt64(epochGauge, r.counter)
+			o.ObserveFloat64(accuracyGauge, r.accuracy)
+			o.ObserveFloat64(lossGauge, r.loss)
+			r.mu.Unlock()
+			return nil
+		},
+		epochGauge,
+		accuracyGauge,
+		lossGauge,
+	)
+	if err != nil {
+		meterProvider.Shutdown(ctx)
+		return nil, fmt.Errorf("register callback failed: %w", err)
+	}
+
+	log.Println("Init metric reporter success")
+	return r, nil
+}
+
+func (r *Reporter) UpdateMetrics(counter int64, loss, accuracy float64) {
+	r.mu.Lock()
+	r.counter = counter
+	r.loss = loss
+	r.accuracy = accuracy
+	r.mu.Unlock()
+}
+
+func (r *Reporter) Shutdown(ctx context.Context) error {
+	return r.provider.Shutdown(ctx)
+}
+
+func (r *Reporter) ForceFlush(ctx context.Context) error {
+	return r.provider.ForceFlush(ctx)
+}
---
+++ main.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"context"
+	"fl_sidecar/exporter"
+	"fl_sidecar/watcher"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+var (
+	metricFile string
+	endpoint   string
+)
+
+func main() {
+	flag.StringVar(&metricFile, "metricfile", "", "Path to the metric file")
+	flag.StringVar(&endpoint, "endpoint", "", "Target endpoint address")
+	flag.Parse()
+
+	if metricFile == "" || endpoint == "" {
+		fmt.Println("Error: -metricfile and -endpoint are required")
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	fmt.Printf("%s, %s\n", metricFile, endpoint)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		<-sigChan
+		log.Println("exiting")
+		cancel()
+	}()
+
+	reporter, err := exporter.NewReporter(ctx, endpoint)
+	if err != nil {
+		log.Fatalf("Init metrics reporter failed: %v", err)
+	}
+	defer reporter.Shutdown(ctx)
+
+	fileWatcher, err := watcher.New(metricFile)
+	if err != nil {
+		log.Fatalf("Init file watcher failed: %v", err)
+	}
+
+	updateChan := fileWatcher.Start(ctx)
+
+	log.Printf("Start watching file %s", metricFile)
+
+	for {
+		select {
+		case content, ok := <-updateChan:
+			if !ok {
+				log.Fatalf("Watcher channel closed")
+				return
+			}
+
+			go parseAndPushMtrics(reporter, string(content))
+
+		case <-ctx.Done():
+			log.Printf("exiting")
+			return
+		}
+	}
+}
+
+func parseAndPushMtrics(reporter *exporter.Reporter, content string) {
+	var epoch int64
+	var loss, accuracy float64
+
+	_, err := fmt.Sscanf(content, "%d,%f,%f", &epoch, &loss, &accuracy)
+	if err != nil {
+		log.Printf("Parse content failed, content: %q, err: %v", string(content), err)
+		return
+	}
+
+	reporter.UpdateMetrics(epoch, loss, accuracy)
+
+	flushCtx, flushCancel := context.WithTimeout(context.Background(), 10*time.Second)
+
+	err = reporter.ForceFlush(flushCtx)
+	if err != nil {
+		log.Printf("Force flush err: %v", err)
+	} else {
+		log.Printf("Push metrics success, epoch: %d, loss: %f, accuracy: %f", epoch, loss, accuracy)
+	}
+
+	flushCancel()
+}
